ca2a7ea0bacb86061e9a1bd99dcb64bbd6948ea1
mdhira <mdhera891@gmail.com>
2021-12-13 22:40:30 -0500

    added version 2 in tokenizing.c

--- tokenizing.c
+++ tokenizing.c
@@ -24,8 +24,26 @@ void tokenizing()
     printf("*** End of Tokenizing Words Demo ***\n\n");
 
     /* Version 2 */
-    >> insert here
+    printf("*** Start of Tokenizing Phrases Demo ***\n");
+    char phrases[200]; // declaring a string of characters of 200 elements
+    char *phrase;
+    int p_counter;                                                   // declaring a integer variable named p_counter
+    printf("Type a few phrases separated by comma(q - to quit):\n"); // prompting the user to enter some phrases
+    gets(phrases);                                                   // accepting the user input
+    while (strcmp(phrases, "q") != 0)                                // while q is not typed, run the next lines of code
+    {
+        phrase = strtok(phrases, "f"); // get the tokens
+        p_counter = 1;                 // p_counter initialized to 1
+        while (phrase)
+        {
+            printf("Phrase #%d is \'%s\'\n", p_counter++, phrase); // the phrase is displayed
+            phrase = strtok(NULL, ",");                            //
+        }
+        printf("Type a few phrases separated by comma(q - to quit):\n");
+        gets(phrases);
+    }
+    printf("*** End of Tokenizing Phrases Demo ***\n\n");
 
-        /* Version 3 */
-        >> insert here
+    /* Version 3 */
+    >> insert here
 }
